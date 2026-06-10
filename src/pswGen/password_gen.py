64c4a3df22aa22c908b2b630c9eff39f5ce93d9f
grenardo <giampytek669@gmail.com>
2026-06-10 17:24:41 +0200

    New method to check the strength of your password

--- src/pswGen/password_gen.py
+++ src/pswGen/password_gen.py
@@ -22,3 +22,38 @@ def gen_psw_editable(length=12, digits=True, punctuation=True, uppercase=True):
         chars += string.punctuation
 
     return ''.join(secrets.choice(chars) for _ in range(length))
+
+'''
+checks the strength of a passwork based on how long and how complex it is.
+'''
+
+def __has_punctation(text):
+    return any(char in string.punctuation for char in text)
+
+def __has_capitalization(text):
+    return any(char.isupper() for char in text)
+
+def __has_digits(text):
+    return any(char.isdigit() for char in text)
+
+def psw_strength_check(password: str):
+    score = 0
+
+    if len(password) >= 8:
+        score += 1
+    if __has_capitalization(password):
+        score += 1
+    if __has_digits(password):
+        score += 1
+    if __has_punctation(password):
+        score += 1
+
+    if score == 4:
+        return "Password molto sicura."
+    elif score == 3:
+        return "Password sicura, ma può essere migliorata."
+    elif score == 2:
+        return "Password debole. Aggiungi maiuscole, numeri o simboli."
+    else:
+        return "Password molto debole. Usa almeno 8 caratteri, una maiuscola, un numero e un simbolo."
+    
