72d5b5670261bddf4dd40db619f7abc1b134f548
grenardo <giampytek669@gmail.com>
2026-06-11 15:42:09 +0200

    Package runnable variable length #4

--- src/pswGen/__main__.py
+++ src/pswGen/__main__.py
@@ -1,4 +1,6 @@
+import sys
 from pswGen.password_gen import gen_psw
 
 if __name__ == "__main__":
-    print(gen_psw(16))
+    length = int(sys.argv[1]) if len(sys.argv) > 1 else 16
+    print(gen_psw(length))
